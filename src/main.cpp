1e756324dce984e2c7d230b9b9bf7ac920777ef0
MrDjeb <MrDjeb245@gmail.com>
2023-05-20 12:26:50 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,10 @@ int main(int argc, char *argv[]) {
     }
 
     cliI.Execute();
-    
+
     //////////////////////////
     auto manager_net = fs::ManagerFilesNet();
-    // manager_net.selectNewFile(fs::F::Path(".gitignore"));
+    manager_net.selectNewFile(fs::F::Path(".gitignore"));
     fs::Buffer buf = manager_net.getBuf();
     for (int i = 0; i < fs::STANDARD_BUFFER_SIZE; i++) {
         std::cout << buf.buf_[i];
